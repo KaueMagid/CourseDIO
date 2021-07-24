912428091b6ab2e17b57a06557c6ca3bf33b6697
KaueMagid <magidsalem96@gmail.com>
2021-07-24 13:58:05 -0300

    First Swagger Setup

--- Course.api/Controllers/UsersController.cs
+++ Course.api/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Course.api.Models.Users;
+using Swashbuckle.AspNetCore.Annotations;
 
 namespace Course.api.Controllers
 {
@@ -12,6 +13,14 @@ namespace Course.api.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {
+        /// <summary>
+        /// Esse serviço permite autenticar um usuário cadastrado e ativo.
+        /// </summary>
+        /// <param name="loginViewModelInput"></param>
+        /// <returns>Retorna o status OK, dados do usuario e o token em caso de sucesso</returns>
+        [SwaggerResponse(statusCode: 200,description:"Sucess", Type =typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 400, description: "Campos Obrigatórios", Type = typeof(LoginViewModelInput))]
+        [SwaggerResponse(statusCode: 500, description: "Error", Type = typeof(LoginViewModelInput))]
         [HttpPost]
         [Route("login")]
         public IActionResult Login(LoginViewModelInput loginViewModelInput)
--- Course.api/Startup.cs
+++ Course.api/Startup.cs
@@ -8,7 +8,9 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace Course.api
@@ -26,6 +28,12 @@ namespace Course.api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -45,6 +53,13 @@ namespace Course.api
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api Course - DIO");
+                c.RoutePrefix = String.Empty;//swagger
             });
         }
     }

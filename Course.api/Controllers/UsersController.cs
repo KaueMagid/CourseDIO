534314945b6edd832dfe942d70c840ecf07af621
KaueMagid <magidsalem96@gmail.com>
2021-07-24 00:10:37 -0300

    Started Controller and Models

--- Course.api/Controllers/UsersController.cs
+++ Course.api/Controllers/UsersController.cs
@@ -4,7 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Course.api.Models.Users
+using Course.api.Models.Users;
 
 namespace Course.api.Controllers
 {

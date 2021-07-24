af4651ffecda3c6fd1a12b3f5a706993edb81f60
KaueMagid <magidsalem96@gmail.com>
2021-07-24 00:07:50 -0300

    Adicionar arquivos de projeto.

---
+++ Course.api/Controllers/UsersController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Course.api.Models.Users
+
+namespace Course.api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Login(LoginViewModelInput loginViewModelInput)
+        {
+            return Created("", loginViewModelInput);
+        }
+    }
+}
---
+++ Course.api/Models/Users/LoginViewModelInput.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Course.api.Models.Users
+{
+    public class LoginViewModelInput
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+
+    }
+}
